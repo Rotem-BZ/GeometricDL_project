64f28d0cddf2d075b28fb838aee517c5b1075c56
nivko <niv.ko@campus.technion.ac.il>
2022-11-06 16:18:56 +0200

    Runs experiments

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+import torch
 import pickle
-import joblib
+# import joblib
 import os
 from os.path import join, isdir
 from typing import Optional
 import time
 from tqdm import tqdm
 
-import torch
 import torch.nn.functional as F
 from torch_geometric.loader import NeighborLoader
 from torch_geometric.nn import MessagePassing, SAGEConv
 from ogb.nodeproppred import Evaluator, PygNodePropPredDataset
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
 
 import wandb
 
@@ -25,28 +26,29 @@ DEPTH_MODEL_PATH = 'depth_model.pkl'
 
 
 class OurDataModule(pl.LightningDataModule):
-    def __init__(self, max_depth, batch_size, neighbor_limit, device='cpu'):
+    def __init__(self, max_depth, batch_size, neighbor_limit, path, device):
         # TODO: implement for cuda
         super(OurDataModule, self).__init__()
+        device = device['accelerator']
         self.max_depth = max_depth
         self.batch_size = batch_size
         self.neighbor_limit = neighbor_limit
         if device == 'cpu':
             self.num_workers = os.cpu_count() - 2
-        elif device == 'cuda':
+        elif device == 'gpu':
             self.num_workers = 5
         else:
             raise ValueError
-
-        self.data, self.num_classes, self.train_idx, self.valid_idx, self.test_idx = [None] * 5
+        self.path = path
+        self.data, self.num_classes, self.train_idx, self.valid_idx, self.test_idx, self.export_idx = [None] * 6
 
     def prepare_data(self) -> None:
         # download
-        _ = PygNodePropPredDataset(name='ogbn-arxiv', root='networks')
+        _ = PygNodePropPredDataset(name='ogbn-arxiv', root=self.path)
 
     def setup(self, stage: Optional[str] = None):
         # stage variable is not used
-        dataset = PygNodePropPredDataset(name='ogbn-arxiv', root='networks')
+        dataset = PygNodePropPredDataset(name='ogbn-arxiv', root=self.path)
         self.data = dataset[0]
         self.data.n_ids = torch.arange(self.data.num_nodes)
         self.data.extracted_features = torch.ones(self.data.num_nodes, 10)  # TODO: use Niv's results
@@ -55,7 +57,8 @@ class OurDataModule(pl.LightningDataModule):
 
         self.train_idx = split_idx['train']
         self.valid_idx = split_idx['valid']
-        self.test_idx = split_idx['test']
+        self.test_idx = split_idx['test'][:10000]
+        self.export_idx = split_idx['test'][10000:]
 
     def train_dataloader(self):
         return NeighborLoader(self.data, input_nodes=self.train_idx,
@@ -67,8 +70,13 @@ class OurDataModule(pl.LightningDataModule):
                               shuffle=False, num_workers=self.num_workers,
                               batch_size=self.batch_size, num_neighbors=[self.neighbor_limit] * self.max_depth)
 
-    def depth_dataloader(self):
+    def test_dataloader(self):
         return NeighborLoader(self.data, input_nodes=self.test_idx,
+                              shuffle=False, num_workers=self.num_workers,
+                              batch_size=self.batch_size, num_neighbors=[self.neighbor_limit] * self.max_depth)
+
+    def depth_dataloader(self):
+        return NeighborLoader(self.data, input_nodes=self.export_idx,
                               shuffle=False, num_workers=self.num_workers,
                               batch_size=self.batch_size, num_neighbors=[self.neighbor_limit] * self.max_depth)
 
@@ -86,7 +94,7 @@ def create_data(network: OurGNN):
 
     with torch.inference_mode():
         for batch in tqdm(loader):
-            features = batch.extracted_features[:batch.batch_size]     # should be (num_nodes, num_features)
+            features = batch.extracted_features[:batch.batch_size]  # should be (num_nodes, num_features)
             targets = network.optimal_depth(batch)
             X.append(features)
             Y += targets.tolist()
@@ -101,66 +109,75 @@ def create_data(network: OurGNN):
         torch.save(Y, file)
 
 
-def train_depth_regressor():
-    from sklearn import tree
-    import numpy as np
-
-    with open(FEATURES_PATH, 'rb') as file:
-        X = torch.load(file).numpy()
-    with open(LABELS_PATH, 'rb') as file:
-        y = torch.load(file).numpy()
-
-    clf = tree.DecisionTreeClassifier()
-    t0 = time.perf_counter()
-    clf.fit(X, y)
-    t1 = time.perf_counter()
-    print(f"time to fit DecisionTreeClassifier: {t1 - t0}")
-    score = clf.score(X, y)
-    print(f"training score: {score}")
-    joblib.dump(clf, DEPTH_MODEL_PATH)
-    print(f"saved trained model to path:\t{DEPTH_MODEL_PATH}")
-    # joblib.load(DEPTH_MODEL_PATH)
+# def train_depth_regressor():
+#     from sklearn import tree
+#     import numpy as np
+#
+#     with open(FEATURES_PATH, 'rb') as file:
+#         X = torch.load(file).numpy()
+#     with open(LABELS_PATH, 'rb') as file:
+#         y = torch.load(file).numpy()
+#
+#     clf = tree.DecisionTreeClassifier()
+#     t0 = time.perf_counter()
+#     clf.fit(X, y)
+#     t1 = time.perf_counter()
+#     print(f"time to fit DecisionTreeClassifier: {t1 - t0}")
+#     score = clf.score(X, y)
+#     print(f"training score: {score}")
+#     joblib.dump(clf, DEPTH_MODEL_PATH)
+#     print(f"saved trained model to path:\t{DEPTH_MODEL_PATH}")
+#     # joblib.load(DEPTH_MODEL_PATH)
 
 
 def main():
     max_depth = 3
     batch_size = 1024
-    epochs = 3
-    neighbor_limit = 30
+    epochs = 150
+    neighbor_limit = -1
     embed_dim = 256
 
-    debug_mode = True
-    device = 'cpu'
-    # device = 'cuda'
+    debug_mode = False
+    # device = {'accelerator': 'cpu'}
+    device = {'accelerator': 'gpu', 'devices': [0]}
+    data_path = '/data/user-data/niv.ko/networks'
 
     if debug_mode:
         wandb_logger = None
         epochs = 1
     else:
-        wandb_logger = WandbLogger(project="geometric_DL")
-        # wandb_logger = WandbLogger(project='geometric_DL', entity='rotem_bz')
+        wandb_logger = WandbLogger(project="geometric_DL", log_model="all", name=f'depth_{max_depth}_epochs_{epochs}')
 
-    data_module = OurDataModule(max_depth, batch_size, neighbor_limit, device=device)
+    run_name = wandb_logger.experiment.name
+
+    data_module = OurDataModule(max_depth, batch_size, neighbor_limit, data_path, device=device)
     data_module.setup()
 
     ###########################################################################
     # train model
+    callbacks = []
+    callbacks.append(ModelCheckpoint(monitor="val_epoch_minimal_loss", mode="min"))
+    callbacks.append(LearningRateMonitor(logging_interval='epoch'))
+    callbacks.append(EarlyStopping(monitor="val_epoch_minimal_loss", mode="min", patience=20))
     model = OurGNN(max_depth, data_module.data.x.shape[1], embed_dim, data_module.num_classes)
-    trainer = pl.Trainer(logger=wandb_logger, max_epochs=epochs)
+    trainer = pl.Trainer(logger=wandb_logger, max_epochs=epochs, **device, callbacks=callbacks,
+                         check_val_every_n_epoch=2)
     trainer.fit(model, data_module)
+    trainer.test(model, data_module)
     ###########################################################################
 
-    ###########################################################################
-    # create optimal-depth data
-    model = OurGNN(max_depth, data_module.data.x.shape[1], embed_dim, data_module.num_classes)
-    model.load_from_checkpoint('checkpoints/epoch=0-step=89-v1.ckpt')
-    create_data(model)
-    ###########################################################################
-
-    ###########################################################################
-    # train decision tree for optimal depth
-    train_depth_regressor()     # note: doesn't require the original data
-    ###########################################################################
+    # ###########################################################################
+    # # create optimal-depth data
+    # model = OurGNN(max_depth, data_module.data.x.shape[1], embed_dim, data_module.num_classes)
+    # model.load_from_checkpoint('checkpoints/epoch=0-step=89-v1.ckpt')
+    # model = OurGNN.load_from_checkpoint('checkpoints/epoch=0-step=89-v1.ckpt')
+    # create_data(model)
+    # ###########################################################################
+    #
+    # ###########################################################################
+    # # train decision tree for optimal depth
+    # train_depth_regressor()     # note: doesn't require the original data
+    # ###########################################################################
 
 
 if __name__ == '__main__':
--- network.py
+++ network.py
@@ -1,12 +1,12 @@
 import os
-
+import wandb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import Adam
 from torch_geometric.loader import NeighborLoader
 from torch_geometric.nn import MessagePassing, SAGEConv
-
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import pytorch_lightning as pl
 
 
@@ -58,7 +58,7 @@ class OurGNN(pl.LightningModule):
 
     def forward(self, batch, batch_idx, state: str):
         losses_list = []    # used for checkpointing
-        losses_sum = torch.tensor(0.0)  # used for backward
+        # losses_sum = torch.tensor(0.0, device=self.device)  # used for backward
         x = batch.x
         y = batch.y.view(-1)[:batch.batch_size]
         edge_index = batch.edge_index
@@ -68,28 +68,57 @@ class OurGNN(pl.LightningModule):
             x = bn_layer(x)
             pred = classifier(x[:batch.batch_size])
             loss = self.loss_func(pred, y)
-            losses_sum += loss
-            losses_list.append(loss.item())
+            # losses_sum += loss
+            losses_list.append(loss)
             x = F.relu(x)
             x = self.dropout(x)
         x = self.layers[-1](x, edge_index)
         pred = self.classifiers[-1](x[:batch.batch_size])
         loss = self.loss_func(pred, y)
-        losses_sum += loss
-        losses_list.append(loss.item())
-        self.log(f'{state}_loss', losses_sum.item())
-        print(f"loss is {losses_sum.item()}")
-        self.log(f'minimal_{state}_loss', min(losses_list))
-        return loss
+        # losses_sum += loss
+        losses_list.append(loss)
+        losses = torch.stack(losses_list)
+        losses_sum = torch.sum(losses)
+        minimal_loss = torch.min(losses)
+
+        self.log(f'{state}_loss', losses_sum.item(), batch_size=batch.batch_size)
+        # print(f"loss is {losses_sum.item()} shape {losses.shape}")
+        self.log(f'minimal_{state}_loss', minimal_loss.item(), batch_size=batch.batch_size)
+        return {'loss': losses_sum, 'minimal_loss': minimal_loss}
+        # return ('loss': loss)
 
     def training_step(self, batch, batch_idx):
-        loss = self(batch, batch_idx, 'train')
-        return loss
+        return self(batch, batch_idx, 'train')
+
+    def epoch_end(self, outputs, stage):
+        if outputs:
+            for metric in outputs[0].keys():
+                if self.trainer.global_step == 0:
+                    wandb.define_metric(f'{stage}_epoch_{metric}', summary='min')
+                losses = torch.stack([output[metric] for output in outputs])
+                mean_loss = torch.mean(losses)
+                self.log(f'{stage}_epoch_{metric}', mean_loss.item())
+
+    def training_epoch_end(self, outputs) -> None:
+        self.epoch_end(outputs, 'train')
+
+    def validation_epoch_end(self, outputs) -> None:
+        self.epoch_end(outputs, 'val')
+
+    def test_epoch_end(self, outputs) -> None:
+        # losses = torch.stack([output['loss'] for output in outputs])
+        # self.logger.experiment.summary['test_loss'] = torch.mean(losses)
+        self.epoch_end(outputs, 'test')
+
 
     def validation_step(self, batch, batch_idx):
-        loss = self(batch, batch_idx, 'val')
+        return self(batch, batch_idx, 'val')
+
+    def test_step(self, batch, batch_idx):
+        return self(batch, batch_idx, 'test')
 
     def configure_optimizers(self):
-        optimizer = Adam(self.parameters())
-        return optimizer
+        optimizer = Adam(self.parameters(), lr=0.03)
+        scheduler = ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.2, min_lr=1e-6)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "train_loss"}
 
